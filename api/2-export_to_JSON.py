239a93a33c65ad0126c0e38276ab6012a80d4356
Sougnabe <s.payang@alustudent.com>
2024-10-18 21:23:20 -0700

    ok

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,10 +7,8 @@ import sys
 
 if __name__ == '__main__':
     employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/".format(employee_id)
 
     user_info = requests.request('GET', user_url).json()
     todos_info = requests.request('GET', todos_url).json()
